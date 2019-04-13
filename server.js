e7e1c6c14944e5c700369377b638944368b3a269
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-04-13 09:16:16 -0600

    Commit 2 - Connected my database, can see json in the browser

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
+const Pokemon = require('./models/pokemon');
+
 const express = require('express'); //from documentation: express is function
 const app = express();//app is an object
+
+app.get('/pokemon', (req, res) => {
+    res.send(Pokemon)
+    })
 
 app.listen(3000, () => {
     console.log("I am listening");
